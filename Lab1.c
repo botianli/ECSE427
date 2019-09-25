6be4b0d2b3ecb61b6c936de8b7cef72df8082b67
TreMansdoerfer <48923439+TreMansdoerfer@users.noreply.github.com>
2019-09-25 16:25:37 -0400

    Messing around

--- Lab1.c
+++ Lab1.c
@@ -1,9 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
 void main() {
 
+    char ansString []="Hello 1";
+    printf(" Middle ");
+    printf(ansString);
+    char getAns [] = get_a_line();
+   // printf("%s", get_a_line());
+    /*
     while (1){
         line = get_a_line();
         if (length(line)>1) {
             my_system(line);
         }
     }
+    */
+}
+
+
+char* get_a_line(){
+    char* ansString = "Hello 2"; 
+    return ansString;
+}
+
+void my_system(){
+
 }
