83d8d044eafba89d1a90833147c5244d51488095
TreMansdoerfer <48923439+TreMansdoerfer@users.noreply.github.com>
2019-10-16 18:33:01 -0400

    Update A2.c

--- Assignment2/A2.c
+++ Assignment2/A2.c
@@ -3,92 +3,58 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-
-static int glob = 0;
-static sem_t sem;
-
-
-static void *threadFunc(void *arg) {
-
-  int loops = *((int *) arg);
-  int loc, j;
-
-  for (j = 0; j < loops; j++) {
-
-     if (sem_wait(&sem) == -1)
-       exit(2);
-
-    loc = glob;
-    loc++;
-    glob = loc;
-
-        if (sem_post(&sem) == -1)
-         exit(2);
-  }
-  return NULL;
-}
-
-
-
-int main(int argc, char *argv[]) {
-
-  pthread_t t1, t2, t3, t4;
-  int s;
-  int loops = 100000;
-
-  if (sem_init(&sem, 0, 1) == -1) {
-    printf("Error, init semaphore\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t1, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t2, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t3, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t4, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_join(t1, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_join(t2, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_join(t3, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_join(t4, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  printf("glob value %d \n", glob);
-  exit(0);
+void * reader(void *) ;  
+void *writer (void *) ;  
+sem_t wsem,mutex ;  
+int readcount=0 ;  
+int c = 0; 
+main()  
+{    
+  printf("%s","clear");  
+  sem_init(&wsem,0,1) ;  
+  sem_init(&mutex,0,1) ;  
+  pthread_t r,w,r1,w1 ;  
+  pthread_create(&r,NULL,reader,(void *)c);  
+  pthread_create(&w1,NULL,writer,(void *)c);  
+  pthread_create(&r1,NULL,reader,(void *)c);  
+  pthread_create(&w,NULL,writer,(void *)c);  
+  pthread_join(r,NULL);  
+  pthread_join(w1,NULL);  
+  pthread_join(r1,NULL);  
+  pthread_join(w,NULL) ;  
+  printf("%s", "main terminated\n");  
+}  
+void * reader(void * arg)  
+{  
+  int d=(int)arg ;  
+  printf("%s %d", "\nreader %d is created", d);  
+  d=d+10;
+     sleep(1);  
+  sem_wait(&mutex) ;  
+     readcount++;  
+     if(readcount==1)  
+         sem_wait(&wsem) ;  
+  sem_post(&mutex) ;  
+/*Critcal Section */  
+  printf("%s %d", "\n\nreader %d is reading\n ",c);  
+  sleep(1) ;  
+  printf("%s %d", "\n\nreader %d is reading\n ",c);  
+/* critical section completd */  
+  sem_wait(&mutex) ;  
+     readcount-- ;  
+     if(readcount==0)  
+         sem_post(&wsem) ;  
+  sem_post(&mutex) ;  
+}  
+void * writer(void * arg)  
+{  
+  int d=(int)arg ;  
+  printf("%s %d", "\nwriter %d is created",d); 
+  arg=(int)arg+10; 
+     sleep(1); 
+  sem_wait(&wsem) ;  
+  printf("%s %d", "\nwriter %d is writing\n" ,d) ;  
+  sleep(1);  
+  printf("%s %d", "\nwriter%d finished writing\n" ,d);  
+  sem_post(&wsem) ;  
 }
