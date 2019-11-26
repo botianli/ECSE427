c4e38bec3c87332108d8c077b8121f42fe5983c9
TreMansdoerfer <48923439+TreMansdoerfer@users.noreply.github.com>
2019-11-26 18:59:13 -0500

    Update and rename disk_emu.h to Assignment3/disk_emu.h

--- Assignment3/disk_emu.h
+++ Assignment3/disk_emu.h
@@ -3,3 +3,4 @@ int init_disk(char *filename, int block_size, int num_blocks);
 int read_blocks(int start_address, int nblocks, void *buffer);
 int write_blocks(int start_address, int nblocks, void *buffer);
 int close_disk();
+
