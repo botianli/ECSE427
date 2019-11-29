879a452b5e760f6deb4ac06e76d75b05a1bea040
TreMansdoerfer <48923439+TreMansdoerfer@users.noreply.github.com>
2019-11-29 00:14:24 -0500

    Update sfs_api.h

--- Assignment3/Tru/sfs_api.h
+++ Assignment3/Tru/sfs_api.h
@@ -5,7 +5,7 @@ int sfs_fopen(char *name);
 int sfs_fclose(int fileID);
 int sfs_fwrite(int fileID, const char *buf, int length);
 int sfs_fread(int fileID, char *buf, int length);
-//int sfs_fseek(int fileID, int offset);
+int sfs_fseek(int fileID, int offset);
 int sfs_fwseek(int fileID, int offset);
 int sfs_frseek(int fileID, int offset);
 int sfs_remove(char *file);
