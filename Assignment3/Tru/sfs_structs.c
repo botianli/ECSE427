f17f8c0b4596e7efd6b6c29931c22ae85d767b78
TreMansdoerfer <48923439+TreMansdoerfer@users.noreply.github.com>
2019-11-29 00:13:42 -0500

    Update sfs_structs.c

--- Assignment3/Tru/sfs_structs.c
+++ Assignment3/Tru/sfs_structs.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "disk_emu.h"
+#include "sfs_api.h"
 #include "sfs_structs.h"
 
 #define MAGIC_NUMBER 256
